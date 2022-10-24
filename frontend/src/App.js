25d651f3525cfdeab1951a732b70033fb2ef44aa
donga-93 <70507672+donga-93@users.noreply.github.com>
2022-10-25 02:05:18 +0400

    Bad Request need correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer/Footer";
 import LandingPage from './screens/LandingPage/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyNotes from './screens/MyNotes/MyNotes';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen/RegisterScreen';
 
 const App = () => (
   <BrowserRouter>
@@ -11,6 +13,8 @@ const App = () => (
    <main>
    <Routes>
       <Route  path="/" element={<LandingPage/>}/>
+      <Route  path="/login" element={<LoginScreen/>}/>
+      <Route  path="/register" element={<RegisterScreen/>}/>
       <Route  path="/mynotes" element={<MyNotes/>}/>
     </Routes>
    </main>
